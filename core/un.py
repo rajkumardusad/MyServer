15c695c2f1ad106c2303ba89328f8cf5cdb6862e
Rajkumar Dusad <32831684+Rajkumrdusad@users.noreply.github.com>
2019-07-23 10:42:43 +0530

    Fixed error

--- core/un.py
+++ core/un.py
@@ -13,21 +13,22 @@ from logo import *
 class Un(object):
   def Uni(self):
     while True:
-		Mylogo()
-		ask = raw_input('''\n\n\033[1;33m Do you want to uninstall MyServer [\033[01;32mY/n\033[01;33m] >> \033[00m''')
-		if ask == "n" or ask == "N":
-			break
-		elif ask == "Y" or ask == "y":
-			os.system("rm -rf "+bpath+"myserver")
-			os.system("cd "+spath+" && rm -rf MyServer")
-			os.system("cd "+spath+" && rm -rf .host.aex .port.aex .path.aex .serv.lock .h.lock")
-			if system=="ubuntu":
-			  os.system("cd "+spath+" && sudo rm -rf MyServer")
-			  os.system("sudo rm -rf /usr/bin/myserver")
-			  os.system("cd "+spath+" && sudo rm -rf .host.aex .port.aex .path.aex .serv.lock .h.lock")
-			exit()
-		else:
-			print("\n \033[01;31m\007Command not found :\033[01;32m \'"+ask+"\'")
-			sleep(1)
+      Mylogo()
+      ask = raw_input('''\n\n\033[1;33m Do you want to uninstall MyServer [\033[01;32mY/n\033[01;33m] >> \033[00m''')
+      if ask == "n" or ask == "N":
+        break
+      elif ask == "Y" or ask == "y":
+        if system=="ubuntu":
+          os.system("cd "+spath+" && sudo rm -rf MyServer")
+          os.system("sudo rm -rf "+bpath+"myserver")
+          os.system("cd "+spath+" && sudo rm -rf .host.aex .port.aex .path.aex .serv.lock .h.lock")
+        else:
+          os.system("rm -rf "+bpath+"myserver")
+          os.system("cd "+spath+" && rm -rf MyServer")
+          os.system("cd "+spath+" && rm -rf .host.aex .port.aex .path.aex .serv.lock .h.lock")
+        exit()
+      else:
+        print("\n \033[01;31m\007Command not found :\033[01;32m \'"+ask+"\'")
+        sleep(1)
 
 Un().Uni()
--- core/upd.py
+++ core/upd.py
@@ -9,42 +9,42 @@ from time import sleep
 from system import *
 from logo import *
 
-class Upd(object):
-  def U(self):
+class upd(object):
+  def chk_upd(self):
     while True:
-		Mylogo()
-		askv = raw_input('''\n\033[1;33m Do you want to Update MyServer [\033[01;32mY/n\033[01;33m] >> \033[00m''')
-		if askv == "n" or askv == "N":
-			break
-		elif askv == "Y" or askv == "y":
-			Mylogo()
-			print("\n\033[01;32mUpdating MyServer .........\033[01;36m")
-			if os.path.exists(bpath+"git"):
-			  print("\033[01;32mDone ....\033[01;36m")
-			else:
-			  os.system(pac+" update")
-			  os.system(pac+" install git -y")
-			if system=="ubuntu":
-			  os.system("cd "+home+" && sudo git clone https://github.com/Rajkumrdusad/MyServer.git")
-			else:
-			  os.system("cd "+home+" && git clone https://github.com/Rajkumrdusad/MyServer.git")
-			if os.path.exists(home+"MyServer"):
-			  os.system("cd "+home+"MyServer && sh install")
-			  Mylogo()
-			  sleep(1)
-			  os.system("clear")
-			  os.system("myserver start")
-			  sys.exit()
-			else:
-			  Mylogo()
-			  print("\n\n\033[01;37m    [+] \033[01;31mSorry We can't update MyServer !!\033[00m")
-			  print("\033[01;37m    [+] \033[01;31mYou are \033[01;33moffline \033[01;31m!!!\033[00m")
-			  print("\033[01;37m    [+] \033[01;31mCheck your \033[01;33mnetwork \033[01;31mconnection !!!\033[00m")
-			  print("\033[01;37m    [+] \033[01;31mTry again after sometime.\033[00m\n\n")
-			  sleep(1)
-			  break
-		else:
-			print("\n \033[01;31m\007Command not found :\033[01;32m \'"+askv+"\'")
-			sleep(1)
+      Mylogo()
+      askv = input("\n\033[1;33m Do you want to Update MyServer [\033[01;32mY/n\033[01;33m] >> \033[1;36m")
+      if askv == "n" or askv == "N":
+        break
+      elif askv == "Y" or askv == "y":
+        Mylogo()
+        print("\n\033[01;32mUpdating MyServer .........\033[01;36m")
+        if os.path.exists(bpath+"git"):
+          print("\033[01;32mDone ....\033[01;36m")
+        else:
+          os.system(pac+" update")
+          os.system(pac+" install git -y")
+        if system=="ubuntu":
+          os.system("cd "+home+" && sudo git clone https://github.com/Rajkumrdusad/MyServer.git")
+        else:
+          os.system("cd "+home+" && git clone https://github.com/Rajkumrdusad/MyServer.git")
+        if os.path.exists(home+"MyServer"):
+          os.system("cd "+home+"MyServer && sh install")
+          os.system("clear")
+          os.system("myserver start")
+          sys.exit()
+        else:
+          Mylogo()
+          print("\n\n\033[01;37m    [+] \033[01;31mSorry We can't update MyServer !!\033[00m")
+          print("\033[01;37m    [+] \033[01;31mYou are \033[01;33moffline \033[01;31m!!!\033[00m")
+          print("\033[01;37m    [+] \033[01;31mCheck your \033[01;33mnetwork \033[01;31mconnection !!!\033[00m")
+          print("\033[01;37m    [+] \033[01;31mTry again after sometime.\033[00m\n\n")
+          sleep(4)
+          break
+      else:
+        print("\n \033[01;31m\007Command not found :\033[01;32m \'"+askv+"\'")
+        sleep(1)
 
-Upd().U()
+def update():
+  upd().chk_upd()
+update()
